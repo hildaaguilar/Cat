cbb5d463d2d40bcc0ada5e80e3e6679e839b5ad1
Aguilar Marquez <aguilhm1@jhuapl.edu>
2020-09-03 15:57:24 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace cat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ()_() ");
+            Console.WriteLine("> ^,^ <");
+            Console.WriteLine("  ( )  ");
+            Console.WriteLine(" (___)__/");
+        }
+
+    }
+}
